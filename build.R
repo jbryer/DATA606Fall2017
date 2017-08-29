34f74c2ec4efe331f44bb4dc89f44cd4c966b018
Added header for item results <JBryer@excelsior.edu>
2017-08-29 15:15:31 -0400

    Update

--- build.R
+++ build.R
@@ -12,6 +12,7 @@ blogdown::install_theme(theme = 'vjeantet/hugo-theme-docdock')
 blogdown::serve_site()
 
 # Build site for Github pages
-blogdown::build_site(method = 'html_encoded'); #file.copy('public', 'docs', recursive = TRUE, overwrite = TRUE);
+blogdown::build_site(method = 'html_encoded'); file.copy('public', 'docs', recursive = TRUE, overwrite = TRUE);
 
+# Sometimes the file.copy doesn't work, then try this (it will replace the entire directory)
 unlink("docs", recursive=TRUE); file.rename('public', 'docs')
